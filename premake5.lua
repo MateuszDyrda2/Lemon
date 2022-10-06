e466327aa535df505161a3aec762efa7be1ad3b1
MateuszDyrda2 <mateusz.dyrda2@gmail.com>
2022-10-06 16:22:12 +0200

    migrated tests

--- premake5.lua
+++
@@ -1,9 +0,0 @@
--- premake5.lua
-workspace "Lemon"
-    configurations { "Debug", "Release" }
-    architecture "x86_64" 
-
-include "Lemon"
-include "Sandbox"
-include "tests"
--- include "Lemonade"
